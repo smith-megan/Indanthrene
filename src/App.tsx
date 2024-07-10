f1fab0d295f7fd8480f282267db7c2ce22eb3b8b
Megan <smith.megan.design@gmail.com>
2024-07-10 16:00:59 -0700

    timeline css and early contact page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import header from "./assets/images/shore.jpg"
 import logo from "./assets/logo.png"
 import drift from "./assets/images/drift.png"
 import rock from "./assets/images/rock.png"
+import beachImage from "./assets/images/beachsteps.png"
+import downloadImage from "./assets/download.png"
 import "./App.css"
 
 function App() {
@@ -11,7 +13,7 @@ function App() {
   return (
     <>
       <div className="font-special grid grid-flow-col align-middle mb-2">
-        <img src={logo} className="self-center w-20"></img>
+        <img src={logo} className="self-center w-20 pl-4 pt-3"></img>
         <h2 className="self-center">Projects</h2>
         <h2 className="self-center">Resume</h2>
         <h2 className="self-center">Contact</h2>
@@ -45,33 +47,72 @@ function App() {
           <div className="row-start-1 col-underlap grid-rows-1;">
             <img src={rock} />
           </div>
-          {/* <div className="row-start-2 col-underlap bg-black w-60 h-40"></div> */}
         </div>
       </div>
       <div className="bg-black grid grid-flow-col gap-4 justify-center p-4">
-        <button className="bg-gray-300 mt-5 rounded-none p-3">.docx</button>
-        <button className="bg-gray-300 mt-5 rounded-none p-2">.pdf</button>
+        <button className="bg-gray-300 mt-5 rounded-none p-3">
+          <img src={downloadImage} className="w-1/12" />
+          .docx
+        </button>
+        <button className="bg-gray-300 mt-5 rounded-none p-2">
+          <img src={downloadImage} className="w-1/12" />
+          .pdf
+        </button>
         <h1 className="text-white p-2">Paper Resume</h1>
       </div>
-      <div>
-        <p>
-          Atlas Technology Engineer 2024-Present Utilized Visual Basic to
-          quickly extract needed data for thousands of products from engineer
-          models in a fraction of required time Organized, edited, combined, and
-          created mechanical drawings and models
-        </p>
-        <p>
-          Scheduling and CRM System Contract Full Stack Programmer 2021-2022
-          Created stream-lined and user-friendly scheduling system with React,
-          Express, Postgres, Docker, Nginx, Bcrypt and Passport JS
-        </p>
-        <p>
-          Dev Mountain Capstone Project 2021 Formulated application to help
-          track data, notes, and personalized tips for insomnia Integrated
-          ChartJS library to visually display data input by users Created
-          PostgreSQL tables to effectively store and organize data with
-          SQLAlchemy as an ORM
-        </p>
+      <div className="timeline relative bg-lightgray after:content-['*'] after:absolute after:h-full after:bg-white after:w-[6px] after:top-0 after:left-1/2 after:ml-[-3px] z-[-1] after:animate-scroll-slow">
+        <div className="container p-3 relative w-96 bg-white opacity-0 animate-move-down [animation-delay: 200ms]">
+          <img
+            className="absolute w-20 right-[-220px] rounded-full"
+            src={rock}
+          />
+          <div className="textbox p-2 relative">
+            <h1>Atlas Technology</h1>
+            <small>2024-present</small>
+            <p>
+              Atlas Technology Engineer 2024-Present Utilized Visual Basic to
+              quickly extract needed data for thousands of products from
+              engineer models in a fraction of required time Organized, edited,
+              combined, and created mechanical drawings and models
+              <span className="h-0 w-0 absolute top-1 border-t-[35px] border-t-transparent border-b-[35px] border-b-transparent border-l-[35px] border-l-white right-[-30px] z-0"></span>
+            </p>
+          </div>
+        </div>
+
+        <div className="container p-2 relative w-96 left-1/2 bg-white opacity-0 animate-move-down delay-700">
+          <img
+            className="absolute w-20 z-10 left-[-220px] rounded-full"
+            src={drift}
+          />
+          <div className="textbox p-2 relative">
+            <h1>CRM Contract</h1>
+            <small>2024-present</small>
+            <p>
+              Scheduling and CRM System Contract Full Stack Programmer 2021-2022
+              Created stream-lined and user-friendly scheduling system with
+              React, Express, Postgres, Docker, Nginx, Bcrypt and Passport JS
+              <span className="h-0 w-0 absolute top-1 border-t-[35px] border-t-transparent border-b-[35px] border-b-transparent border-r-[35px] border-r-white left-[-30px] z-0"></span>
+            </p>
+          </div>
+        </div>
+        <div className="container p-2 relative w-96 left-1/2 bg-white opacity-0 animate-move-down delay-700">
+          <img
+            className="absolute w-20 z-10 left-[-220px] rounded-full"
+            src={drift}
+          />
+          <div className="textbox p-2 relative">
+            <h1>CRM Contract</h1>
+            <small>2024-present</small>
+            <p>
+              Dev Mountain Capstone Project 2021 Formulated application to help
+              track data, notes, and personalized tips for insomnia Integrated
+              ChartJS library to visually display data input by users Created
+              PostgreSQL tables to effectively store and organize data with
+              SQLAlchemy as an ORM
+              <span className="h-0 w-0 absolute top-1 border-t-[35px] border-t-transparent border-b-[35px] border-b-transparent border-r-[35px] border-r-white left-[-30px] z-0"></span>
+            </p>
+          </div>
+        </div>
         <p>
           Smith Design & Technology Web Designer, User Interface Design, and
           Graphic Design 2019 to 2021 Partnered with a Senior Dev and
@@ -81,6 +122,18 @@ function App() {
           experience and email contact integration for a startup company to
           build their web presence
         </p>
+      </div>
+      <div>
+        <h1>Projects</h1>
+        <p>projects here</p>
+      </div>
+      <div className="grid grid-cols-2">
+        <img src={beachImage} />
+        <div>
+          <h1>Contact</h1>
+          <form></form>
+          <button>Send</button>
+        </div>
       </div>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,24 @@ export default {
         overlap: "1 / span 7",
         underlap: "4 / -1",
       },
+      animation: {
+        "scroll-slow": "scroll 4s linear forwards",
+        "move-down": "movedown 2s linear forwards",
+      },
+      keyframes: {
+        scroll: {
+          "0%": { height: 0 },
+          "100%": { height: "100%" },
+          // "50%": { transform: "rotate(3deg)" },
+        },
+        movedown: {
+          "0%": { opacity: 1, transform: "translateY(-30px)" },
+          "100%": { opacity: 1, transform: "translateY(0px)" },
+        },
+      },
+      colors: {
+        lightgray: "#D6D5D3",
+      },
     },
   },
   plugins: [],
